05b4788c9924acfa2a3402f7611c56c275c7b206
Carlos <216593078+carlosmoscardo@users.noreply.github.com>
2025-09-23 15:36:18 +0200

    Object computed properties

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-let user = {
-    age: 30,
+let fruit = prompt("Which food do you want to buy?", "apple");
+let bag = {
+    [fruit]: 5,
+    [fruit + "Computers"]: 1
 }
+console.log(bag.apple); // does work
+console.log(bag["apple"]); // does work
+console.log(bag.appleComputers); // does work
+console.log(bag["appleComputers"]); // does work
 
-let test = "age";
-
-console.log(user.test); // does not work
-console.log(user[test]); // does work
+let newFruit = "banana";
+let newBag = {
+    newFruit: 10, // does not work
+    [newFruit]: 10, // does work
+}
+console.log(newBag.banana);
