6e472d9f4da2f9d3d165fb5679065c0518b9bb3d
Carlos <216593078+carlosmoscardo@users.noreply.github.com>
2025-09-23 16:35:37 +0200

    tackling non-existent keys and undefined values

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-let user = {};
+let user = {
+    firstName: "James",
+    age: 30,
+    test: undefined,
+}
 
-console.log(user.name); // returns undefined because the property doesn't exist
+console.log(user.test); // undefined puede dar a entender que la propiedad no existe
+console.log("test"in user); // por eso usamos el operador IN
+
+// obj.key te devuelve undefined cuando esa key no existe
+// pero si da la casualidad de que justamente una key tiene asociado el valor "undefined", entonces puedes pensar que esa clave no existe cuando realmente sí existe
+// en ese tipo de situaciones usamos el operador IN
+// la de IN es preguntar directamente si existe la clave en el objeto
+// la sintaxis en cuestión es: "clave" in obj
+// "clave" siempre en quotes
+
+let animal = {
+    type: "dog",
+    age: 5,
+}
+
+console.log("test" in animal); // false
+console.log("type" in animal, "age" in animal); // true true
+
+// Si no queremos poner quotes, podemos usar variables
+let example = "age";
+console.log(example in animal); // true
