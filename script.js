2c616bd3032c7a9cce4e914afabd5dfe173bc407
Carlos <216593078+carlosmoscardo@users.noreply.github.com>
2025-09-23 14:48:01 +0200

    object properties using quotes

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let user =  {
     name: "John",
     age: 32,
     location: "USA",
+    "max height": "180cm",
+    "second name": "Fraud"
 }
 
 // Las claves son strings aunque no se escriban como tal
