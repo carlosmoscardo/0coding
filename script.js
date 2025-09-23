b7165669977ca853a8cc316c6c06b05d6c567931
Carlos <216593078+carlosmoscardo@users.noreply.github.com>
2025-09-23 16:27:25 +0200

    non-existent object propeties return undefined

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// Crea un objeto numbers con una clave 0 y valor "cero". Demuestra que se puede acceder tanto con numbers[0] como con numbers["0"].
+let user = {};
 
-let obj = {
-    0: "cero",
-}
-
-console.log(obj[0]);
-console.log(obj["0"]);
+console.log(user.name); // returns undefined because the property doesn't exist
