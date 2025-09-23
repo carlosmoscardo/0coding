01eb79df3275f24e2eac3d8eea6449a79df1d44c
Carlos <216593078+carlosmoscardo@users.noreply.github.com>
2025-09-23 18:58:42 +0200

    object exercise

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-const family = {
-    names: ["Ana", "John"],
-    ages: [14, 39],
-    sayHi() {
-        return `Hi my name is ${this.names[0]} and I am ${this.ages[0]}. My father's name is ${this.names[1]} and he's ${this.ages[1]}.`
-    },
-    direccion: {
-        calle: "Calle Castilla",
-        numero: 11,
-        ciudad: "Madrid",
-    },
+// Crea un objeto llamado car que tenga las propiedades brand, model, y un método start() que imprima “El coche está arrancando”.
+
+const car = {
+    brand: "Ferrari",
+    model: "F40",
+    start() {
+        console.log("El coche está arrancando");
+    }
 }
 
-console.log(family.sayHi());
+car.start();
