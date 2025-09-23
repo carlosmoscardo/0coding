58501b0439d9a0ee078c0f1f8cbe75b28a06d37c
Carlos <216593078+carlosmoscardo@users.noreply.github.com>
2025-09-23 17:39:06 +0200

    solve object exercise

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-let salaries = {
-    John: 100,
-    Ann: 160,
-    Pete: 130,
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu",
 }
 
-let sum = 0;
-
-for (let key in salaries) {
-    sum += salaries[key];
+function multiplyNumeric(obj) {
+    for (let key in obj) {
+        if (typeof obj[key] === "number") {
+            obj[key] *= 2;
+        }
+    }
 }
 
-/* 
-Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
-If salaries is empty, then the result must be 0.
-*/
+multiplyNumeric(menu);
 
 
