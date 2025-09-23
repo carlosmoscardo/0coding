b8dd2a74a08aba79ac3ba26e838241f1fce2223a
Carlos <216593078+carlosmoscardo@users.noreply.github.com>
2025-09-23 17:22:42 +0200

    object exercise

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-// Create an empty object user.
-// Add the property name with the value John.
-// Add the property surname with the value Smith.
-// Change the value of the name to Pete.
-// Remove the property name from the object.
+// Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
 
-let user = {
-    name: "John",
-    surname: "Smith",
-};
+// let schedule = {};
+// alert( isEmpty(schedule) ); // true
+// schedule["8:30"] = "get up";
+// alert( isEmpty(schedule) ); // false
 
-user.name = "Pete";
-delete user.name;
+function isEmpty(obj) {
+    let i = 0;
+    for (let key in obj) {
+        if (key in obj === true) {
+            console.log(false);
+            i++;
+        } 
+    }
+    if (i === 0) {
+        console.log(true);
+    }
+}
+
+let test = {};
+
+isEmpty(test);
