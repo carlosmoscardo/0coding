4542f9b2ebc460ff96c625a136a7450a8fc8167f
Carlos <216593078+carlosmoscardo@users.noreply.github.com>
2025-09-25 10:51:16 +0200

    solve array exercise

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-function camelize(str) {
-    return str.split("-").map((val, idx) => {
-        if (idx === 0) {
-            return val;
-        } else {
-            return (val[0].toUpperCase() + val.slice(1));
-        }
-    }).join("");
+let arr = [5, 3, 8, 1];
+
+function filterRange(arr, a, b) {
+    return arr.filter((num) => num >= a && num <= b);
 }
 
-console.log(camelize("background-color")); // == 'backgroundColor'
-console.log(camelize("list-style-image")); // == 'listStyleImage'
-console.log(camelize("-webkit-transition")); // == 'WebkitTransition'
-// That is: removes all dashes, each word after dash becomes uppercased.
+let filtered = filterRange(arr, 1, 4);
+
+console.log(( filtered )); // 3,1 (matching values)
+
+console.log(( arr )); // 5,3,8,1 (not modified)
