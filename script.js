6029d533c7b088bfff21f353d3d70a3ce608922e
Carlos <216593078+carlosmoscardo@users.noreply.github.com>
2025-10-03 16:11:43 +0200

    Solve exercise

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-// function camelize(str) {
-//     return str
-//         .split("-")
-// }
+function camelize(str) {
+    const arr = str.split("-");
+    const newArr = arr.map((val, idx) => idx === 0 ? val : val[0].toUpperCase() + val.slice(1));
+    const final = newArr.join("");
+    return final;
+}
 
-// console.log(camelize("background-color"))
-// console.log(camelize("list-style-image"))
-// console.log(camelize("-webkit-transition"))
+console.log(camelize("background-color"));
+console.log(camelize("list-style-image"));
+console.log(camelize("-webkit-transition"));
 
-// camelize("background-color") == 'backgroundColor';
-// camelize("list-style-image") == 'listStyleImage';
-// camelize("-webkit-transition") == 'WebkitTransition';
+camelize("background-color") == 'backgroundColor';
+camelize("list-style-image") == 'listStyleImage';
+camelize("-webkit-transition") == 'WebkitTransition';
 
-let str = "camel-case";
-let result = str.split("-");
-console.log(result);
+
