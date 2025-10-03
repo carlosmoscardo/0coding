99eb7720880e4f7bb37aaeafb148e7e4d0f0cfd6
Carlos <216593078+carlosmoscardo@users.noreply.github.com>
2025-10-03 15:58:41 +0200

    split() array method

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+// function camelize(str) {
+//     return str
+//         .split("-")
+// }
 
+// console.log(camelize("background-color"))
+// console.log(camelize("list-style-image"))
+// console.log(camelize("-webkit-transition"))
+
+// camelize("background-color") == 'backgroundColor';
+// camelize("list-style-image") == 'listStyleImage';
+// camelize("-webkit-transition") == 'WebkitTransition';
+
+let str = "camel-case";
+let result = str.split("-");
+console.log(result);
