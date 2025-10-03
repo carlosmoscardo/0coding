380af87e897f1e7c36984c472e3cb9f9dcc64ab2
Carlos <216593078+carlosmoscardo@users.noreply.github.com>
2025-10-03 17:41:29 +0200

    Solve array exercise

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-let arr = [1, 2, 3];
-
-function shuffle(array) {
-    array.sort(() => Math.random() - 0.5);
+function unique(arr) {
+    return arr.reduce((acc, val) => {
+        if (!acc.includes(val)) {
+            acc.push(val);
+        }
+        return acc;
+    }, []);
 }
 
-shuffle(arr);
-console.log(arr);
-shuffle(arr);
-console.log(arr);
-shuffle(arr);
-console.log(arr);
-shuffle(arr);
-console.log(arr);
+let strings = ["Hare", "Krishna", "Hare", "Krishna", "Krishna", "Krishna", "Hare", "Hare", ":-O"];
+
+console.log(unique(strings)); // Hare, Krishna, :-O
